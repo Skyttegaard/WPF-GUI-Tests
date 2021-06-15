2392b49b0be9fc491f6ff4fe526d84cf7cabfbab
Daniel <Daniel@LAPTOP-QVGQ2KAM>
2021-06-15 14:35:07 +0200

    Add project files.

---
+++ Engine/ViewModels/Viewmodels.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Timers;
+using System.Windows;
+namespace Engine.ViewModels
+{
+    public class Viewmodels : BaseNotificationClass
+    {
+        private string _currentTime = "00:00";
+
+        public Viewmodels()
+        {
+            
+        }
+        public string CurrentTime
+        {
+            get => _currentTime;
+            set
+            {
+                _currentTime = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+        public void ButtonTimeout()
+        {
+        }
+
+    }
+}
---
+++ GUI til test program/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using System.Diagnostics;
+using System.ComponentModel;
+using System.Timers;
+
+using Engine.ViewModels;
+
+namespace GUI_til_test_program
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DispatcherTimer dispatcherTimer = new DispatcherTimer();
+        Stopwatch stopWatch = new Stopwatch();
+        Viewmodels ViewModel;
+        
+        public int buttonTimer_Tick { get; private set; }
+
+        public MainWindow()
+        {
+            dispatcherTimer.Tick += new EventHandler(DispatcherTimer_Tick);
+            dispatcherTimer.Interval = new TimeSpan(0,  0,  0,  0,  1);
+            ViewModel = new Viewmodels();
+            DataContext = ViewModel;
+            InitializeComponent();
+        }
+        
+        private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            
+        }
+        private void LavFejl_Click(object sender, RoutedEventArgs e)
+        {
+            stopWatch.Start();
+            dispatcherTimer.Start();
+            DelayButton(sender,e);
+        }
+        private void RetFejl_Click(object sender, RoutedEventArgs e)
+        {
+            DelayButton(sender,  e);
+        }
+        private void DispatcherTimer_Tick(object sender, EventArgs e)
+        {
+            if (stopWatch.IsRunning)
+            {
+                TimeSpan ts = stopWatch.Elapsed;
+                ViewModel.CurrentTime = string.Format("{0:00}:{1:00}", ts.Minutes, ts.Seconds);
+            }
+        }
+        private async void DelayButton(object sender, EventArgs e)
+        {
+            UIElement element = (UIElement)sender;
+
+            element.IsEnabled = false;
+            await Task.Delay(5000);
+            element.IsEnabled = true;
+        }
+        
+    }
+}
